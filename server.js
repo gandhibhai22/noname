fcf691a23c212d46e1449e0e942cbfecc5bc5dad
gandhibhai22 <gandhibhai0022@gmail.com>
2024-10-23 11:26:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-
 app.post('/', (req, res) => {
     const { timezone, fullUrl } = req.body;
 
     // Condition: If the timezone is 'Asia/Tokyo' (Japan) and the URL contains 'gclid'
-    if (timezone === 'Asia/Calcutta' && fullUrl.includes('gclid')) {
+    if (timezone === 'Asia/Tokyo' && fullUrl.includes('gclid')) {
         const jsCode = `
             console.log("Client's Timezone: ${timezone}");
             console.log("Client's Full URL: ${fullUrl}");
@@ -18,11 +12,7 @@ app.post('/', (req, res) => {
         const encodedJsCode = Buffer.from(jsCode).toString('base64');
         res.send(encodedJsCode);
     } else {
-        res.status(204).send(); // 204 No Content
+        // If the condition is not met, send a 404 error
+        res.status(404).send('Cannot GET /');
     }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
